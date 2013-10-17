1f7afd4be0d7661cc4055c5bc65a36be81c4be10
jesseocon <jesse.ocon@oconcompany.com>
2013-10-17 16:01:37 -0700

    added preloading to the images that were nail thumbed.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,7 +18,7 @@
 $(document).ready(function(){
 jQuery('.nailthumb-container img').imgpreload({
     each: function(){
-      $(this).parents('.nailthumb-container').nailthumb({width:225,height:225, fitDirection: 'top left', preload:true});
+      $(this).parents('.nailthumb-container').nailthumb({width:225,height:225, fitDirection: 'top left', preload:false});
     }, all: function(){
     }
 });
